bf60a5005112fbaaacd96a6727e77f375b5b42e1
Franzi <62101565+fransie@users.noreply.github.com>
2022-06-28 14:25:11 +0200

    Send error output of rm operations in clean.sh to log.

--- test/clean.sh
+++ test/clean.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# create dirs
+mkdir -p obj
+mkdir -p logs
+
 # remove artifacts from last test run
-rm obj/*
-rm logs/*
-rm -r peasoup_executable_directory*
+rm obj/* > /dev/null 2>&1
+rm logs/* > /dev/null 2>&1
+rm -r peasoup_executable_directory* > /dev/null 2>&1
