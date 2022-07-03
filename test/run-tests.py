e8507da4bc451e8327ce657df226bdb61a55609d
Franzi <62101565+fransie@users.noreply.github.com>
2022-07-03 14:56:12 +0200

    Make test script concurrent.

--- test/run-tests.py
+++ test/run-tests.py
@@ -3,6 +3,7 @@ import subprocess
 import sys
 from os import listdir
 from os.path import isfile, join
+import concurrent.futures
 
 CLEAN_SCRIPT = "./clean.sh"
 GREEN = '\033[92m'
@@ -51,6 +52,37 @@ def run_test(filename):
     return subprocess.call(f"./{sanitized_name} >> logs/{test_name}.txt 2>&1", shell=True)
 
 
+def execute_test_case(file):
+    if not file.endswith(".cpp"):
+        return
+    if regex != "":
+        result = re.search(regex, file)
+        if result is None:
+            return
+    if is_disabled(file):
+        return
+    build(file)
+    sanitize(file)
+    exit_code = run_test(file)
+    if exit_code == 2:
+        print(f"Run of sanitized {file} failed.")
+        return
+
+    expected_output = get_expected_output(file)
+    log = file.removesuffix(".cpp") + ".txt"
+    log_lines = open(f"logs/{log}", "r").readlines()
+
+    line_num = 1
+    for line in log_lines:
+        if expected_output in line:
+            print(f"******* {file} *******\n{GREEN}SUCCESS: Found expected output in logs/{log}. Expected: '{expected_output}'{END}")
+            break
+        if line_num == len(log_lines):
+            print(f"******* {file} *******\n{RED}ERROR: Expected output was not in logs/{log}. Expected: '{expected_output}'{END}")
+            break
+        line_num += 1
+
+
 if __name__ == '__main__':
     subprocess.call(CLEAN_SCRIPT, shell=True)
 
@@ -59,34 +91,5 @@ if __name__ == '__main__':
         regex = sys.argv[1]
     # TODO: fix absolute path
     testfiles = [f for f in listdir("/home/franzi/Documents/binary-msan/test") if isfile(join("", f))]
-    testfiles.sort()
-    for file in testfiles:
-        if not file.endswith(".cpp"):
-            continue
-        if regex != "":
-            result = re.search(regex, file)
-            if result is None:
-                continue
-        print(f"******* {file} *******")
-        if is_disabled(file):
-            continue
-        build(file)
-        sanitize(file)
-        exit_code = run_test(file)
-        if exit_code == 2:
-            print(f"Run of sanitized {file} failed.")
-            continue
-
-        expected_output = get_expected_output(file)
-        log = file.removesuffix(".cpp") + ".txt"
-        log_lines = open(f"logs/{log}", "r").readlines()
-
-        line_num = 1
-        for line in log_lines:
-            if expected_output in line:
-                print(f"{GREEN}SUCCESS: Found expected output in logs/{log}. Expected: '{expected_output}'{END}")
-                break
-            if line_num == len(log_lines):
-                print(f"{RED}ERROR: Expected output was not in logs/{log}. Expected: '{expected_output}'{END}")
-                break
-            line_num += 1
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        executor.map(execute_test_case, testfiles)
