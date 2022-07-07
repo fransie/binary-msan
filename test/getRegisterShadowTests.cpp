a5572a91ea2213b3885bfd94f26c743ed4ece135
Franzi <62101565+fransie@users.noreply.github.com>
2022-07-07 13:50:28 +0200

    Implement getRegisterShadowTests.

--- test/getRegisterShadowTests.cpp
+++ test/getRegisterShadowTests.cpp
@@ -1,4 +1,3 @@
-#include <cassert>
 #include <iostream>
 #include <msan.h>
 #include "gtest/gtest.h"
@@ -10,8 +9,56 @@ TEST(getRegisterShadowTests, width64){
     EXPECT_EQ(shadowRegisterState[0].to_ullong(), UINT64_MAX);
 
     // when
-    auto result = (uint64_t) getRegisterShadow(0, QUAD_WORD);
+    auto result = static_cast<uint64_t*>(getRegisterShadow(0, QUAD_WORD));
 
     // then
-    EXPECT_EQ(result, UINT64_MAX);
+    EXPECT_EQ(*result, UINT64_MAX);
+}
+
+TEST(getRegisterShadowTests, width32){
+    // given
+    shadowRegisterState[0].set();
+    EXPECT_EQ(shadowRegisterState[0].to_ullong(), UINT64_MAX);
+
+    // when
+    auto result = static_cast<uint32_t*>(getRegisterShadow(0, DOUBLE_WORD));
+
+    // then
+    EXPECT_EQ(*result, UINT32_MAX);
+}
+
+TEST(getRegisterShadowTests, width16){
+    // given
+    shadowRegisterState[0].set();
+    EXPECT_EQ(shadowRegisterState[0].to_ullong(), UINT64_MAX);
+
+    // when
+    auto result = static_cast<uint16_t*>(getRegisterShadow(0, WORD));
+
+    // then
+    EXPECT_EQ(*result, UINT16_MAX);
+}
+
+TEST(getRegisterShadowTests, width8){
+    // given
+    shadowRegisterState[0].set();
+    EXPECT_EQ(shadowRegisterState[0].to_ullong(), UINT64_MAX);
+
+    // when
+    auto result = static_cast<uint8_t*>(getRegisterShadow(0, BYTE));
+
+    // then
+    EXPECT_EQ(*result, UINT8_MAX);
+}
+
+TEST(getRegisterShadowTests, width8Highh){
+    // given
+    shadowRegisterState[0] = std::bitset<64>{0xffffffffffffff00};
+    EXPECT_EQ(shadowRegisterState[0].to_ullong(), UINT64_MAX - 255);
+
+    // when
+    auto result = static_cast<uint8_t*>(getRegisterShadow(0, HIGHER_BYTE));
+
+    // then
+    EXPECT_EQ(*result, UINT8_MAX);
 }
