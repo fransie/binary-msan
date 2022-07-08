b7533ae119c62ec15511bcf5a95a09213e8d80fe
Franzi <62101565+fransie@users.noreply.github.com>
2022-07-08 17:37:13 +0200

    Add other reg to reg tests.

---
+++ test/MovHandlerTests/regToReg_8bitHigh.cpp
@@ -0,0 +1,24 @@
+// COMPILE OPTIONS: -I/home/franzi/Documents/llvm-project-llvmorg-13.0.1/compiler-rt/lib/msan -I/home/franzi/Documents/llvm-project-llvmorg-13.0.1/compiler-rt/include/sanitizer/ -I/home/franzi/Documents/llvm-project-llvmorg-13.0.1/compiler-rt/lib/  -L/home/franzi/Documents/binary-msan/plugins_install -linterface
+
+#include <assert.h>
+#include <iostream>
+#include <cstdint>
+#include "../../runtimeLibrary/Interface.h"
+
+int main() {
+    // given
+    // define rax here because "new" is not instrumented yet and returns an uninit address is rax, which is wrong.
+    setRegShadow(true,0,64);
+    assert(shadowRegisterState[0].to_ullong() == 0);
+    assert(shadowRegisterState[1].to_ullong() == UINT64_MAX);
+
+    // when
+    asm ("mov %ah, %ch");
+
+    // then
+    assert(shadowRegisterState[1].to_ullong() == 0xffffffffffff00ff);
+    std::cout << "Success." << std::endl;
+    return 0;
+}
+
+// EXPECTED: Success.
