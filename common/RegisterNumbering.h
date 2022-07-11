5b0ca745ef72e80fd1831d593ebb4c958f39e062
Franzi <62101565+fransie@users.noreply.github.com>
2022-07-11 13:43:50 +0200

    Add RegisterNumbering enum for easy access to reg numbers.

---
+++ common/RegisterNumbering.h
@@ -0,0 +1,22 @@
+#ifndef BINARY_MSAN_REGISTERNUMBERING_H
+#define BINARY_MSAN_REGISTERNUMBERING_H
+
+enum RegisterNumbering{
+    RAX = 0,
+    RCX = 1,
+    RDX = 2,
+    RSP = 4,
+    RBP = 5,
+    RSI = 6,
+    RDI = 7,
+    R8 = 8,
+    R9 = 9,
+    R10 = 10,
+    R11 = 11,
+    R12 = 12,
+    R13 = 13,
+    R14 = 14,
+    R15 = 15
+};
+
+#endif //BINARY_MSAN_REGISTERNUMBERING_H
