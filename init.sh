e5e628b122e30114a408a67302fdc3ac91332316
fransie <62101565+fransie@users.noreply.github.com>
2022-09-19 16:10:52 +0200

    Delete init.sh

--- init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# init env vars
-current=$PWD
-cd ${ZIPR_PATH}
-source set_env_vars
-cd $current
-export PSPATH=$PSPATH:$BINMSAN_HOME/plugins_install
-export CAPSTONE=${PEASOUP_HOME}/irdb-libs/third_party/capstone/include/capstone
