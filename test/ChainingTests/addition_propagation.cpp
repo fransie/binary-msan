67c965a1b3f9115cf7704bafbfb2f12a8b8c49ac
Franzi <62101565+fransie@users.noreply.github.com>
2022-10-18 17:03:12 +0200

    Add test cases for propagation and libc sink.

---
+++ test/ChainingTests/addition_propagation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    printf("This should be printed.\n");
+    uint64_t a = 5;
+    uint64_t b;
+    a = a + b;
+    if (a)
+        printf("This should not be printed.\n");
+    return 0;
+}
+
+// EXPECTED: MemorySanitizer: use-of-uninitialized-value
--- test/SinkTests/libc.cpp
+++ test/SinkTests/libc.cpp
@@ -1,20 +1,15 @@
 // COMPILE OPTIONS: -I$BINMSAN_HOME/llvm_shared_msan_lib/compiler-rt/lib/msan -I$BINMSAN_HOME/llvm_shared_msan_lib/compiler-rt/include/sanitizer/ -I$BINMSAN_HOME/llvm_shared_msan_lib/compiler-rt/lib/  -L$BINMSAN_HOME/plugins_install -lbinmsan_lib
 
+#include <unistd.h>
 #include <iostream>
-#include <vector>
 #include "../../src/runtimeLibrary/BinMsanApi.h"
 #include "../../src/common/RegisterNumbering.h"
-
-class UumClass{
-public:
-    char *text = new char[10];
-};
 
 int main() {
     // define rax here because "new" is not instrumented yet and returns an uninit address in rax, which is wrong.
     setRegShadow(true,RAX,64);
-    auto x = new UumClass;
-    std::cout << x->text << std::endl;
+    char *ptr = new char[10];
+    std::cout << ptr << std::endl;
     return 0;
 }
 
