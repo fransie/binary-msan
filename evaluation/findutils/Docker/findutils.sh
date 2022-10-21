ec56ac67b743578f359f00e33da9ca8716e25006
Franzi <62101565+fransie@users.noreply.github.com>
2022-10-21 17:00:40 +0200

    Build findutils with -O2 as well.

--- evaluation/findutils/Docker/findutils.sh
+++ evaluation/findutils/Docker/findutils.sh
@@ -9,15 +9,13 @@ curl -LO http://ftp.gnu.org/gnu/findutils/findutils-${find_version}.tar.xz
 tar -xf findutils-${find_version}.tar.xz
 
 export CC=/usr/bin/x86_64-alpine-linux-musl-gcc
-export CFLAGS="-static"
 
 cd findutils-${find_version}
-CFLAGS="$CFLAGS -Os -ffunction-sections -fdata-sections" LDFLAGS='-Wl,--gc-sections' ./configure --without-bash-malloc
+CFLAGS="${CFLAGS} -static -O2 -ffunction-sections -fdata-sections" LDFLAGS='-Wl,--gc-sections' ./configure
 make
 mkdir binaries
 cp xargs/xargs binaries/
 cp find/find binaries/
 cp locate/locate binaries/
 cp locate/updatedb binaries/
-popd # find-${find_version}
-popd
+cd ../..
