a9dec49c4cb9addc7a8bfd1fbbc9e4d42fdfc368
Franzi <62101565+fransie@users.noreply.github.com>
2022-10-25 16:28:03 +0200

    Measure run-time performances as well.

--- evaluation/time_measurement.py
+++ evaluation/time_measurement.py
@@ -7,8 +7,8 @@ import pandas as pd
 from functools import reduce
 from operator import add
 
-TEST_DIRECTORY = os.getcwd().removesuffix("evaluation") + "test"
 EVAL_DIRECTORY = os.getcwd()
+TEST_DIRECTORY = EVAL_DIRECTORY.removesuffix("evaluation") + "test"
 BIN_DIRECTORY = EVAL_DIRECTORY + "/bin"
 SAN_DIRECTORY = EVAL_DIRECTORY + "/san"
 MSAN_DIRECTORY = EVAL_DIRECTORY + "/msan"
@@ -18,9 +18,8 @@ TEST_FOLDERS = [name for name in os.listdir(TEST_DIRECTORY)
 
 
 class Tool(Enum):
-    Regular = 1
-    Memcheck = 2
-    Dr_Memory = 3
+    Memcheck = 1
+    Dr_Memory = 2
 
 
 class Compile(Enum):
@@ -29,10 +28,10 @@ class Compile(Enum):
 
 
 def get_env():
-    e = dict(os.environ)
-    e.update({'TIMEFORMAT': 'real %3R'})
-    e.update({'LC_NUMERIC': 'en_US.UTF-8'})
-    return e
+    env = dict(os.environ)
+    env.update({'TIMEFORMAT': 'real %3R'})
+    env.update({'LC_NUMERIC': 'en_US.UTF-8'})
+    return env
 
 
 def get_test_source_files():
@@ -50,8 +49,11 @@ def get_test_source_files():
 
 
 def append_averages(results):
-    for folder in ["ChainingTests", "StackVariableHandlerTests"]:
-    #for folder in TEST_FOLDERS:
+    test_cases_in_folder = [v for k, v in results.items()]
+    average = reduce(add, test_cases_in_folder) / len(test_cases_in_folder)
+    results["OVERALL-AVERAGE"] = average
+    #for folder in ["ChainingTests", "StackVariableHandlerTests"]:
+    for folder in TEST_FOLDERS:
         test_cases_in_folder = [v for k, v in results.items() if k.startswith(folder)]
         average = reduce(add, test_cases_in_folder) / len(test_cases_in_folder)
         results[f"{folder}-AVERAGE"] = average
@@ -90,7 +92,7 @@ def measure_sanitization_time(binaries, with_binmsan: bool):
         file = binary_path.split("/")[-1]
         if with_binmsan:
             pathlib.Path(SAN_DIRECTORY).mkdir(exist_ok=True)
-            sanitize_command = f"bash -i -c 'time ../binary-msan.sh {binary_path} {SAN_DIRECTORY}/{file}_san'"
+            sanitize_command = f"bash -i -c 'time ../binary-msan.sh {binary_path} {SAN_DIRECTORY}/{file}'"
         else:
             try:
                 dict(os.environ)["PSZ"]
@@ -113,10 +115,10 @@ def measure_sanitization_time(binaries, with_binmsan: bool):
 
 
 # binmsan & msan
-def measure_run_time_performance(binmsanified_binaries):
+def measure_static_run_time_performance(binaries):
     results = {}
-    for binmsanified_binary in binmsanified_binaries:
-        run_command = f"bash -i -c 'time {binmsanified_binary}'"
+    for binary in binaries:
+        run_command = f"bash -i -c 'time {binary}'"
         sum = 0
         runs = 10
         for i in range(runs):
@@ -129,19 +131,46 @@ def measure_run_time_performance(binmsanified_binaries):
                 if line.startswith("real"):
                     time = line.replace("real ", "")
                     sum += float(time)
-        results[binmsanified_binary] = round((sum / runs), 3)
+        file = binary.split("/")[-1]
+        results[file] = round((sum / runs), 3)
+    append_averages(results)
     return results
 
 
+# regular, memcheck and dr memory
+def measure_dynamic_runtime_performance(binaries, tool: Tool):
+    results = {}
+    if tool == Tool.Memcheck:
+        cmd = "valgrind --tool=memcheck"
+    elif tool == Tool.Dr_Memory:
+        cmd = "drmemory --"
+    else:
+        print(f"Unknown tool {tool}. Abort.")
+        exit(1)
+    for binary in binaries:
+        file = binary.split("/")[-1]
+        run_command = f"bash -i -c 'time {cmd} {binary}'"
+        sum = 0
+        runs = 10
+        for i in range(runs):
+            subprocess_return = subprocess.Popen(run_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                                 env=get_env(),
+                                                 shell=True, text=True)
+            stdout, stderr = subprocess_return.communicate()
+            # Time outputs its measurement in seconds to stderr
+            for line in stderr.split("\n"):
+                if line.startswith("real"):
+                    time = line.replace("real ", "")
+                    sum += float(time)
+        results[file] = round((sum / runs), 3)
+    append_averages(results)
+    return results
 
 
-if __name__ == '__main__':
-    # Prepare folders
-    pathlib.Path('bin').mkdir(exist_ok=True)
-    pathlib.Path('msan').mkdir(exist_ok=True)
-
+def get_compile_and_sanitization_data():
     # Get source files
-    test_sources = get_test_source_files()[0:2] + [get_test_source_files()[-1]]
+    test_sources = get_test_source_files()
+    print(f"Start compilation and sanitization.")
 
     # Compilation: Clang vs. Clang & MSan
     build_time_clang = measure_build_time(test_sources, Compile.Regular)
@@ -165,22 +194,68 @@ if __name__ == '__main__':
         'zipr-binmsan': sanitization_time_binmsan_zipr,
         'clang+binmsan': build_and_sanitize_time_binmsan
     })
-    df = dataframe.sort_index()
+    print(f"Finished compilation and sanitization.")
+    return dataframe.sort_index()
 
-    #
-    # # binmsanified_binaries = [join(SAN_DIRECTORY, file) for file in os.listdir(SAN_DIRECTORY) if isfile(join(SAN_DIRECTORY, file))]
-    #
-    # # Run-time performance
-    # # msan_res = measure_run_time_performance(binmsanified_binaries)
-    # reg_res = measure_dynamic_runtime_performance(binaries,Tool.Regular)
-    # memcheck_res = measure_dynamic_runtime_performance(binaries, Tool.Memcheck)
-    # dr_res = measure_dynamic_runtime_performance(binaries, Tool.Dr_Memory)
-    #
-    # print("REGULAR:")
-    # print(reg_res)
-    # print("Memcheck:")
-    # print(memcheck_res)
-    # print("Dr Memory:")
-    # print(dr_res)
 
-    # Persist results
+def get_run_time_performance():
+    print(f"Start run-time performance measurement.")
+    # Check all programs are available.
+    exit_code = subprocess.call("which drmemory > /dev/null", shell=True)
+    exit_code += subprocess.call("which valgrind > /dev/null", shell=True)
+    if exit_code != 0:
+        print("Please make sure that the executables valgrind and drmemory are available. Abort.")
+        exit(1)
+
+    # get binaries
+    binaries = [join(BIN_DIRECTORY, file) for file in os.listdir(BIN_DIRECTORY) if isfile(join(BIN_DIRECTORY, file))]
+    msanified_binaries = [join(MSAN_DIRECTORY, file) for file in os.listdir(MSAN_DIRECTORY) if
+                          isfile(join(MSAN_DIRECTORY, file))]
+    binmsanified_binaries = [join(SAN_DIRECTORY, file) for file in os.listdir(SAN_DIRECTORY) if
+                             isfile(join(SAN_DIRECTORY, file))]
+
+    # get performance
+    baseline_run_time = measure_static_run_time_performance(binaries)
+    msan_run_time = measure_static_run_time_performance(msanified_binaries)
+    binmsan_run_time = measure_static_run_time_performance(binmsanified_binaries)
+    memcheck_run_time = measure_dynamic_runtime_performance(binaries, Tool.Memcheck)
+    drmemory_run_time = measure_dynamic_runtime_performance(binaries, Tool.Dr_Memory)
+
+    # return dataframe
+    dataframe = pd.DataFrame.from_dict({
+        'baseline': baseline_run_time,
+        'msan': msan_run_time,
+        'binmsan': binmsan_run_time,
+        'dr memory': drmemory_run_time,
+        'memcheck': memcheck_run_time,
+    })
+    print(f"Finished run-time performance measurement.")
+    return dataframe.sort_index()
+
+
+if __name__ == '__main__':
+    # Prepare folders
+    pathlib.Path('bin').mkdir(exist_ok=True)
+    pathlib.Path('msan').mkdir(exist_ok=True)
+
+    # Get source files
+    test_sources = get_test_source_files()[0:2] + [get_test_source_files()[-1]]
+
+    # Compilation: Clang vs. Clang & MSan
+    build_time_clang = measure_build_time(test_sources, Compile.Regular)
+    build_time_clang_msan = measure_build_time(test_sources, Compile.MSan)
+
+    # Instrumentation: Zipr vs. Zipr & BinMSan
+    binaries = [join(BIN_DIRECTORY, file) for file in os.listdir(BIN_DIRECTORY)]
+    sanitization_time_binmsan_zipr = measure_sanitization_time(binaries, with_binmsan=True)
+    sanitization_time_zipr = measure_sanitization_time(binaries, with_binmsan=False)
+
+    print(f"Processing {len(get_test_source_files())} test cases.")
+    # ######## Compilation and sanitization
+    # df = get_compile_and_sanitization_data()
+    # df.to_csv(f"{result_path}/compile_sanitize_times.csv", sep=',')
+
+    ######### Run-time performance
+    df = get_run_time_performance()
+    df.to_csv(f"{result_path}/run_time_performances.csv", sep=',')
+
